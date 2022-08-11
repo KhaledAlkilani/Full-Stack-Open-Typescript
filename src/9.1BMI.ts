eede232265d0c726347d23c8f627736d35522907
Khaled <khaledkelany@gmail.com>
2022-08-11 10:24:10 +0300

    go

---
+++ src/9.1BMI.ts
@@ -0,0 +1,13 @@
+const bmi = (weight: number, height: number) => {
+
+    if (weight / Math.pow(height, 2) * 10000 < 18.5){
+        return 'Under weight'
+    } else if (weight / Math.pow(height, 2) * 10000 > 18.5 && weight / Math.pow(height, 2) * 10000 < 24.9){
+        return 'Normal ' + '(healthy weight)'
+    } else if (weight / Math.pow(height, 2) * 10000 > 25 && weight / Math.pow(height, 2) * 10000 < 29.9){
+        return 'Over weight'
+    } else {
+        return 'Obesity'
+    }
+}
+console.log(bmi(40, 180))
